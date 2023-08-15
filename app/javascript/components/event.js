e4ab6e82a3bfdbc5cbb4c1c89dca6febb190ec42
Avalon van der Horst <avalon@Avalons-MacBook-Air.local>
2023-08-15 14:18:44 +0200

    add event and event-list components

--- app/javascript/components/event.js
+++ app/javascript/components/event.js
@@ -1,10 +1,50 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPencilAlt, faTrash } from '@fortawesome/free-solid-svg-icons'
+// import {far} from "@fortawesome/free-regular-svg-icons"
+// import {fas} from "@fortawesome/free-solid-svg-icons"
+// import {fab} from "@fortawesome/free-brands-svg-icons"
+// import {library} from "@fortawesome/fontawesome-svg-core"
+// import "@fortawesome/fontawesome-free"
+// library.add(far, fas, fab)
 
-const Event = ({event}) => React.createElement(
-  'div',
-  {className: 'event'},
-  React.createElement('h2', null, event.title),
-  React.createElement('p', null, event.description)
-);
+const Event = ({event}) => {
+  const date = new Date(event.start_date); // use start_date
+
+  const weekday = date.toLocaleString('en-US', {weekday: 'short'}).toUpperCase();;
+
+  const day = date.getDate();
+
+  return React.createElement(
+    'div',
+    {className: 'event-card'},
+    React.createElement('div', {className: 'start-day'},
+      React.createElement(
+        'div', {key: 'weekday', className: 'weekday'}, weekday
+      ),
+      React.createElement(
+        'div', {key: 'day', className: 'month-day'}, day
+      )
+    ),
+
+    React.createElement('div', {className: 'event-content'},
+      React.createElement('h2', {className: 'event-title'}, event.title),
+      React.createElement('p', {className: 'event-descr'}, event.description)
+    ),
+
+    React.createElement(
+      'div',
+      {className: 'actions'},
+      React.createElement(
+        'a', {className: 'edit-btn', href: `/events/${event.id}/edit`},
+        React.createElement(FontAwesomeIcon, {icon: faPencilAlt})
+      ),
+      React.createElement(
+        'a', {className: 'delete-btn', href: `/events/${event.id}/delete`},
+        React.createElement(FontAwesomeIcon, {icon: faTrash})
+      )
+    )
+  )
+};
 
 export default Event
--- app/javascript/components/events.js
+++ app/javascript/components/events.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import Event from './event'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 const Events = ({events}) => {
   return React.createElement(
     'div',
     {className: 'events-list'},
-
-    // Map over events and render Event component
+    React.createElement('div', {className: "heading"},
+      React.createElement(
+        'h1',
+        {className: 'header mt-3 mb-5'},
+        'Your upcoming events:',
+      ),
+      React.createElement('div', {className: 'create-event mt-3 mb-5'},
+        React.createElement(
+          'a',
+          {className: 'new-btn', href: `/events/new`},
+          React.createElement(FontAwesomeIcon, {icon: faPlus})
+        ),
+      )
+    ),
     events.map(event => {
       return React.createElement(Event, {key: event.id, event: event})
     })
