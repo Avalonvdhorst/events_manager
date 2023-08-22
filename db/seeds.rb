2049f86cdc8978200d5163a6e7bd05e8220b7443
Avalon van der Horst <avalon@Avalons-MacBook-Air.local>
2023-08-22 16:06:04 +0200

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 Event.destroy_all
 
-Event.create(title: 'Project setup',
+Event.create(title: 'Final project Factorial',
   description: 'Finish the project for the technical interview with Factorial',
   start_date: Date.new(2023, 8, 22),
   end_date: Date.new(2023, 8, 22))
 
 Event.create(title: 'Housewarming',
   description: 'Housewarming of Dianas new flat in Gracia',
-  start_date: Date.new(2023, 8, 22),
-  end_date: Date.new(2023, 8, 22))
+  start_date: Date.new(2023, 8, 29),
+  end_date: Date.new(2023, 8, 29))
 
 Event.create(title: 'Birthday',
   description: 'My own 35th birthday party, a picnic on the beach',
